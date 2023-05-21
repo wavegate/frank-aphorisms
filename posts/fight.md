ab67349bbb82bb6c7eb45887cfd7440311f36e60
Frank Lee <flee63@gatech.edu>
2023-05-21 01:07:13 -0400

    commit

---
+++ posts/fight.md
@@ -0,0 +1,6 @@
+---
+title: "Fight"
+date: "2021-05-19"
+---
+
+Fight as if you are already dead.
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,33 @@
+import styles from "./styles.module.scss";
+
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Posts() {
+  const data = await getData();
+  return (
+    <div>
+      <h1>Posts</h1>
+      {data.map((post: any) => {
+        return (
+          <div key={post.id}>
+            <div className={`${styles["boxy"]}`}>UserId: {post.userId}</div>
+            <div>Title: {post.title}</div>
+            <div>Body: {post.body}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
